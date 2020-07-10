fa1041e695d3b617374738fbe1c0ce6e47e7c05c
Dev Govindji <friendsofdev94@gmail.com>
2020-07-10 15:39:11 -0500

    completes tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,7 +24,92 @@ export class AppComponent {
       title: "Sunset while Camping",
       url: 'https://images.unsplash.com/photo-1563299796-17596ed6b017?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80',
     },
+    {
+      title: "At the Beach",
+      url: 'https://images.unsplash.com/photo-1509233725247-49e657c54213?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1887&q=80',
+    },
+    {
+      title: "Going for a Night Drive",
+      url: 'https://images.unsplash.com/photo-1503376780353-7e6692767b70?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80',
+    },
+    {
+      title: "Hanging out at the Game",
+      url: 'https://images.unsplash.com/photo-1585189240757-e219296c9164?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1767&q=80',
+    },
+    {
+      title: "Sunset while Camping",
+      url: 'https://images.unsplash.com/photo-1563299796-17596ed6b017?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80',
+    },
+    {
+      title: "At the Beach",
+      url: 'https://images.unsplash.com/photo-1509233725247-49e657c54213?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1887&q=80',
+    },
+    {
+      title: "Going for a Night Drive",
+      url: 'https://images.unsplash.com/photo-1503376780353-7e6692767b70?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80',
+    },
+    {
+      title: "Hanging out at the Game",
+      url: 'https://images.unsplash.com/photo-1585189240757-e219296c9164?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1767&q=80',
+    },
+    {
+      title: "Sunset while Camping",
+      url: 'https://images.unsplash.com/photo-1563299796-17596ed6b017?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80',
+    },
+    {
+      title: "At the Beach",
+      url: 'https://images.unsplash.com/photo-1509233725247-49e657c54213?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1887&q=80',
+    },
+    {
+      title: "Going for a Night Drive",
+      url: 'https://images.unsplash.com/photo-1503376780353-7e6692767b70?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80',
+    },
+    {
+      title: "Hanging out at the Game",
+      url: 'https://images.unsplash.com/photo-1585189240757-e219296c9164?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1767&q=80',
+    },
+    {
+      title: "Sunset while Camping",
+      url: 'https://images.unsplash.com/photo-1563299796-17596ed6b017?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80',
+    },
+    {
+      title: "At the Beach",
+      url: 'https://images.unsplash.com/photo-1509233725247-49e657c54213?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1887&q=80',
+    },
+    {
+      title: "Going for a Night Drive",
+      url: 'https://images.unsplash.com/photo-1503376780353-7e6692767b70?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80',
+    },
+    {
+      title: "Hanging out at the Game",
+      url: 'https://images.unsplash.com/photo-1585189240757-e219296c9164?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1767&q=80',
+    },
+    {
+      title: "Sunset while Camping",
+      url: 'https://images.unsplash.com/photo-1563299796-17596ed6b017?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80',
+    },
+    {
+      title: "At the Beach",
+      url: 'https://images.unsplash.com/photo-1509233725247-49e657c54213?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1887&q=80',
+    },
+    {
+      title: "Going for a Night Drive",
+      url: 'https://images.unsplash.com/photo-1503376780353-7e6692767b70?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80',
+    },
+    {
+      title: "Hanging out at the Game",
+      url: 'https://images.unsplash.com/photo-1585189240757-e219296c9164?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1767&q=80',
+    },
+    {
+      title: "Sunset while Camping",
+      url: 'https://images.unsplash.com/photo-1563299796-17596ed6b017?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80',
+    },
   ]
 
+
+  checkWindowIndex(index: number) {
+    return Math.abs(this.currentPage - index) < 5;
+
+  }
 
 }
